f38a5ff651e0abfd187bcd70091e4c2a019933c6
= <=>
2020-07-30 17:41:55 +0300

    added filter-app

---
+++ precache-manifest.3a6343118d102c82eaaa3ad04bc98420.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b85405a414ee2346522b0f2b18914858",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98c6aecb70a5a399f058",
+    "url": "/static/js/2.2f360f68.chunk.js"
+  },
+  {
+    "revision": "b21c0b070fbf389a149e773557d68a8a",
+    "url": "/static/js/2.2f360f68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebd9df29d9ce6cdebf9f",
+    "url": "/static/js/main.09e696ca.chunk.js"
+  },
+  {
+    "revision": "5d0cd10dcd90081f708b",
+    "url": "/static/js/runtime-main.a36e04f5.js"
+  }
+]);
